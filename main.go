ab754d822b2b096f67deedaf8d6e934e85ad41d3
Flavio Francisco <flaviofrancis@msn.com>
2024-07-31 20:18:14 +0200

    feat: Add Gin server with /ping endpoint
    This commit adds a Gin server to the main.go file and implements a /ping endpoint that returns a JSON response with the message "pong". This is the initial implementation of the server.

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	server := gin.Default()
+
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	},)
+
+	server.Run(":8080")
 }
