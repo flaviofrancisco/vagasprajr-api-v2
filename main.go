02792065ec04e8cfa69c3772d3fb953c5bf7380f
Flavio Francisco <flaviofrancis@msn.com>
2024-08-05 16:03:45 +0200

    chore: Update main.go logging statements to include MongoDB environment variables

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/flaviofrancisco/vagasprajr-api-v2/routes"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -18,11 +15,11 @@ func main() {
 	}
 	
 	// print the environment variables
-	log.Println("DEBUG_MODE:", os.Getenv("DEBUG_MODE"))
-	log.Println("VERSION:", os.Getenv("VERSION"))
-	log.Println("BASE_UI_HOST:", os.Getenv("BASE_UI_HOST"))
-	log.Println("MONGODB_URL:", os.Getenv("MONGODB_URL"))
-	log.Println("MONGODB_DATABASE:", os.Getenv("MONGODB_DATABASE"))	
+	// log.Println("DEBUG_MODE:", os.Getenv("DEBUG_MODE"))
+	// log.Println("VERSION:", os.Getenv("VERSION"))
+	// log.Println("BASE_UI_HOST:", os.Getenv("BASE_UI_HOST"))
+	// log.Println("MONGODB_URL:", os.Getenv("MONGODB_URL"))
+	// log.Println("MONGODB_DATABASE:", os.Getenv("MONGODB_DATABASE"))	
 
 	server := gin.Default()
 
