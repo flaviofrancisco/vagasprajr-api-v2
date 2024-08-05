104aeda6441549f723001bd0708b0cd6c48c415a
Flavio Francisco <flaviofrancis@msn.com>
2024-08-05 16:10:45 +0200

    chore: Update main.go logging statements to include MongoDB environment variables

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"os"
+	"regexp"
 
 	"github.com/flaviofrancisco/vagasprajr-api-v2/routes"
 	"github.com/gin-gonic/gin"
@@ -17,11 +19,21 @@ func main() {
 	}
 	
 	// print the environment variables
-	// log.Println("DEBUG_MODE:", os.Getenv("DEBUG_MODE"))
-	// log.Println("VERSION:", os.Getenv("VERSION"))
-	// log.Println("BASE_UI_HOST:", os.Getenv("BASE_UI_HOST"))
-	// log.Println("MONGODB_URL:", os.Getenv("MONGODB_URL"))
-	// log.Println("MONGODB_DATABASE:", os.Getenv("MONGODB_DATABASE"))	
+	log.Println("DEBUG_MODE:", os.Getenv("DEBUG_MODE"))
+	log.Println("VERSION:", os.Getenv("VERSION"))
+	log.Println("BASE_UI_HOST:", os.Getenv("BASE_UI_HOST"))
+
+	mongoDbUrl := os.Getenv("MONGODB_URL")
+
+	if mongoDbUrl == "" {
+		log.Fatal("MONGODB_URL is required")
+	}
+
+	re := regexp.MustCompile(`(mongodb://.*?:)(.*?)(@.*)`)
+	modifiedURL := re.ReplaceAllString(mongoDbUrl, `$1*.*.*$3`)
+
+	log.Println("MONGODB_URL:", modifiedURL)
+	log.Println("MONGODB_DATABASE:", os.Getenv("MONGODB_DATABASE"))	
 
 	server := gin.Default()
 
