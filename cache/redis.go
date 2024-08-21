883996018ca31d1697b3434c45c30df54dd28369
Flavio Francisco <flaviofrancis@msn.com>
2024-08-21 23:29:38 +0200

    chore: Update Redis connection code
    The code changes in `redis.go` update the Redis connection code to include the Redis server address and a truncated version of the Redis password in the connection log message. This improves logging and troubleshooting.
    
    Co-authored-by: Flavio Francisco

--- cache/redis.go
+++ cache/redis.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"errors"
+	"fmt"
 	"os"
 
 	"github.com/go-redis/redis"
@@ -12,11 +13,20 @@ type Redis struct {
 }
 
 func NewRedis() (Redis, error) {
+
     var client = redis.NewClient(&redis.Options{
         Addr:     os.Getenv("REDIS_SERVER"),
         Password: os.Getenv("REDIS_PASSWORD"), 
 		DB:	   0,
     })
+
+    var redis_password string
+    
+    if len(os.Getenv("REDIS_PASSWORD")) > 4 {
+        redis_password = os.Getenv("REDIS_PASSWORD")[0:4]
+    }
+
+    fmt.Println("Connecting to Redis server: ", os.Getenv("REDIS_SERVER") + " with password: " + redis_password)
 
     _, err := client.Ping().Result()
     if err != nil {
